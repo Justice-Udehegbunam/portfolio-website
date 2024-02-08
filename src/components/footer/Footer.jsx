3e5a9b2c0c50dd81c79aac104e2508534e8215ce
Justice-Udehegbunam <justicefavour491@gmail.com>
2024-02-08 23:18:46 +0100

    90% done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,35 @@
 import "./footer.scss";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentYear(new Date().getFullYear());
+    }, 1000 * 60); // Update every minute to ensure timely year changes
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <p>&copy; {currentYear} Justice. All rights reserved.</p>
+        </div>
+        <a href="#" onClick={scrollToTop}>
+          Back to Top
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
